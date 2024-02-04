7e289f86d6ee289a8d7ad3c1556f2813f643229c
Juan David Baena <juan.baenaz@upb.edu.co>
2024-02-03 19:20:18 -0500

    reintento de correccion linea 46y50

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -43,11 +43,11 @@ int main(int argc, char *argv[]) {
     /*Print the result*/
     if(count == 1)
     {
-        printf("%d is a prime number\n",number);
+        printf(stderr"%d is a prime number\n",number);
     }
     else
     {
-        printf("%d is not a prime number\n",number);
+        printf(stdout"%d is not a prime number\n",number);
     }
 
     /*Print the result*/
